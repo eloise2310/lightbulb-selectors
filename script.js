0428d0a572e00c5882e283b8ec48eb5ca1c12bc2
eloise <eloisewilkinson@Eloises-MacBook-Pro.local>
2024-09-26 12:43:06 +0100

    lightbulb upto if statement

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 // Write your code here
+
+const lightbulb1 = document.querySelector("#lightbulb1");
+const lightbulb2 = document.querySelector("#lightbulb2");
+const lightbulb3 = document.querySelector("#lightbulb3");
+const subtitle = document.querySelector("h3.subtitle");
+let count = 0
+
+lightbulb1.addEventListener("click", function(){
+    count++
+    if(count===1){
+        subtitle.innerHTML = "You've clicked the light 1 time"
+    }else{
+    subtitle.innerHTML = "You've clicked the lights " + count + " times" 
+    }
+    lightbulb1.classList.toggle("active");
+})
+
+lightbulb2.addEventListener("click", function(){
+    count++
+    if(count===1){
+        subtitle.innerHTML = "You've clicked the light 1 time"
+    }else{
+    subtitle.innerHTML = "You've clicked the lights " + count + " times" 
+    }
+    lightbulb2.classList.toggle("active");
+})
+
+lightbulb3.addEventListener("click", function(){
+    count++
+    if(count===1){
+        subtitle.innerHTML = "You've clicked the light 1 time"
+    }else{
+    subtitle.innerHTML = "You've clicked the lights " + count + " times" 
+    }
+    lightbulb3.classList.toggle("active");
+})
+
+
+
